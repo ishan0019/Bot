21ce94cee387a6eecf381bb3416db44510b07f68
ishan0019 <ishan.sh19@gmail.com>
2019-11-21 16:47:03 +0530

    initialize

---
+++ bot.py
@@ -0,0 +1,66 @@
+import requests
+import sys
+import bs4
+import discord
+from discord.ext import commands
+from connections import SearchHistory
+
+client = commands.Bot(command_prefix = '')
+
+@client.event
+async def on_ready():
+	print('Bot is ready..')
+
+
+@client.command(aliases=['Hi','HI','hI'])
+async def hi(ctx):
+	await ctx.send(f'hey!')
+
+@client.command(aliases=['!google'])
+async def google(ctx, *, question):
+	print(ctx.message.author)
+	print(ctx.message.author.id)
+	# print(client.user)
+	sh = SearchHistory(ctx.message.author, ctx.message.author.id)
+	sh.setMessage(question)
+	# sh.getMessage()
+	linkList = searchGoogle(question)
+	await ctx.send(f'{question}?\nAnswer\n{linkList}')
+
+headers_Get = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1'
+    }
+
+def searchGoogle(query):
+	
+    s = requests.Session()
+    url = 'https://www.google.com/search?q='+''.join(query)+'&ie=utf-8&oe=utf-8'
+    res = s.get(url, headers=headers_Get)
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    linkElements = soup.select('.r a')
+    output = []
+    linkCount = min(5, len(linkElements))
+    for i in range(linkCount):
+    	output.append(linkElements[i].get('href'))
+    	
+    return output
+
+
+@client.command(aliases=['!recent'])
+async def recent(ctx, *, question):
+	
+	sh = SearchHistory(ctx.message.author, ctx.message.author.id)
+	history = []
+	history = list(sh.getMessage(question))
+	resString = ' '.join([str(elem) for elem in history]) 
+
+	await ctx.send(f'{resString}')
+
+
+client.run('NjQ2NjEzOTEwNTY4NDM1NzIy.XdTsqw.p1TonfjVdBExFYTN0KRl5JkjVic')
---
+++ connections.py
@@ -0,0 +1,61 @@
+import psycopg2
+
+class SearchHistory:
+	def __init__(self, user, id):
+		self.user = user
+		self.userId = id
+		self.connection = None
+		self.cursor = None
+
+	def connectionStart(self):
+		con = psycopg2.connect(user = "postgres",
+                                  password = "postgres1x2x",
+                                  host = "database-1.c01msz8wnl0d.us-east-2.rds.amazonaws.com",
+                                  port = "5432",
+                                  database = "database-1")
+		
+		return con
+
+	def connectionClose(self):
+		if self.connection:
+			self.cursor.close()
+			self.connection.close()
+			print("PostgreSQL connection is closed")
+
+	def setMessage(self, log):
+		if log:
+			try:
+				self.connection = self.connectionStart()
+				self.cursor = self.connection.cursor()		
+				timestamp = ''
+				query = "INSERT INTO BotHistory (userId, username, log) VALUES (%s,%s,%s)"
+				print(self.userId)
+				print(self.user)
+				record = (self.userId, str(self.user), str(log))
+				self.cursor.execute(query, record)
+				self.connection.commit()
+				print ("Record inserted successfully")
+
+			except (Exception, psycopg2.Error) as error :
+			    if(self.connection):
+			        print("Failed to insert record into BotHistory table", error)
+
+			finally:
+				self.connectionClose()
+			        
+
+	def getMessage(self, key):
+		try:
+			self.connection = self.connectionStart()
+			self.cursor = self.connection.cursor()
+			query = "select log from BotHistory where userId=%s and log like %s"
+			self.cursor.execute(query, (self.userId, str(key)))
+			record = self.cursor.fetchone()
+			print(record)
+			return record
+		except (Exception, psycopg2.Error) as error :
+		    if(self.connection):
+		        print("Failed to fetch record from BotHistory table", error)
+
+		finally:
+			self.connectionClose()
