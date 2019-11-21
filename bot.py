9ecd612830eee1d41bbdd554b1a9104d3ee0e99f
ishan0019 <ishan.sh19@gmail.com>
2019-11-21 22:30:32 +0530

    bug fi

--- bot.py
+++ bot.py
@@ -24,32 +24,32 @@ async def google(ctx, *, question):
 	sh = SearchHistory(ctx.message.author, ctx.message.author.id)
 	sh.setMessage(question)
 	linkList = searchGoogle(question)
-    resString = ' '.join([str(elem) for elem in linkList]) 
+	resString = ' '.join([str(elem) for elem in linkList])
 	await ctx.send(f'{question}?\nAnswer\n{resString}')
 
 headers_Get = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Accept-Encoding': 'gzip, deflate',
-        'DNT': '1',
-        'Connection': 'keep-alive',
-        'Upgrade-Insecure-Requests': '1'
-    }
+		'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0',
+		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+		'Accept-Language': 'en-US,en;q=0.5',
+		'Accept-Encoding': 'gzip, deflate',
+		'DNT': '1',
+		'Connection': 'keep-alive',
+		'Upgrade-Insecure-Requests': '1'
+	}
 
 def searchGoogle(query):
 	
-    s = requests.Session()
-    url = 'https://www.google.com/search?q='+''.join(query)+'&ie=utf-8&oe=utf-8'
-    res = s.get(url, headers=headers_Get)
-    soup = bs4.BeautifulSoup(res.text, 'html.parser')
-    linkElements = soup.select('.r a')
-    output = []
-    linkCount = min(5, len(linkElements))
-    for i in range(linkCount):
-    	output.append(linkElements[i].get('href'))
-    	
-    return output
+	s = requests.Session()
+	url = 'https://www.google.com/search?q='+''.join(query)+'&ie=utf-8&oe=utf-8'
+	res = s.get(url, headers=headers_Get)
+	soup = bs4.BeautifulSoup(res.text, 'html.parser')
+	linkElements = soup.select('.r a')
+	output = []
+	linkCount = min(5, len(linkElements))
+	for i in range(linkCount):
+		output.append(linkElements[i].get('href'))
+		
+	return output
 
 
 @client.command(aliases=['!recent'])
